08e0b53ac09498e22c81e9a8c083bd8241c6b489
syu-h81 <58079996+syu-h81@users.noreply.github.com>
2020-07-07 22:29:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -13,11 +13,38 @@ $(function() {
 $(function() {
   $(window).scroll(function(){
     var y = $(this).scrollTop();
-    $('#ls-bg').css('background-position', '0 ' + parseInt( -y / 5 ) + 'px');
-    $('#ls').css('background-position', '0 ' + parseInt( -y / 4 ) + '%');
+    $('#ls-bg').css('background-position', '0 ' + parseInt( -y / 4 ) + 'px');
+    $('#ls').css('background-position', '0 ' + parseInt( -y / 8 ) + '%');
     //$('#bg2-ls-img').css('background-position', '0 ' + parseInt( -y / 3 ) + 'px');
     //$('#safety-bg').css('background-position', '0 ' + parseInt( -y / 7 ) + 'px');
     //$('#bg02').css('background-position', '0 ' + parseInt( -y / 50 ) + 'px');
     //$('#bg03').css('background-position', '0 ' + parseInt( -y / 200 ) + 'px');
   });
 });
+
+window.addEventListener('scroll',function(){
+  header_scroll();
+  parallax('top-wrapper-block',5,0);
+});
+
+function header_scroll(){
+  var my_header = document.getElementsByTagName('header')[0];
+  var scroll_height = window.pageYOffset;
+  if(scroll_height > 400){
+    my_header.className = 'header header-scroll';
+  }
+  else {
+    my_header.className = 'header';
+  }
+}
+
+function parallax(elem,speed,number){
+  var target = document.getElementsByClassName(elem)[number];
+  var scroll_value = window.pageYOffset;
+  if(scroll_value <= 700){
+    target.style.transform = 'translateY('+(-scroll_value/speed)+'px)';
+  }
+  else {
+    return;
+  }
+}
